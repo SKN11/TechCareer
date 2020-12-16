69860ee7aa7096cafe3818dd55601b166e489177
KHAN Sameer <SKN11@dsone.3ds.com>
2020-12-16 23:43:54 +0530

    cnm spring boot project with rest api

--- JAVA/SpringBoot/Projects/crm/src/main/java/com/crm/model/ReactAndSpringApplication.java
+++ JAVA/SpringBoot/Projects/crm/src/main/java/com/crm/model/ReactAndSpringApplication.java
@@ -1,7 +1,13 @@
 package com.crm.model;
 
+import java.util.Locale;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.AcceptHeaderLocaleResolver;
 
 @SpringBootApplication
 public class ReactAndSpringApplication {
@@ -13,4 +19,23 @@ public class ReactAndSpringApplication {
 
 	}
 
+	@Bean
+	public LocaleResolver localeResolver()
+	{
+		//SessionLocaleResolver localeResolver = new SessionLocaleResolver();
+		AcceptHeaderLocaleResolver localeResolver = new AcceptHeaderLocaleResolver();
+		localeResolver.setDefaultLocale(Locale.US);
+		return localeResolver;
+	}
+	
+	//put logging.level.org.springframework=info  
+	//spring.messages.basename=messages   in properties file in plcae of below mwthod
+//	@Bean  
+//	public ResourceBundleMessageSource messageSource()  
+//	{  
+//	ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();  
+//	messageSource.setBasename("messages");  
+//	return messageSource;  
+//	}  
+	
 }
